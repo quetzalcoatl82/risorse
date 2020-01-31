897c6bdc4bf7817ce2d82906a524fe8b5db42841
Federico Lelli <federico.lelli@mondadori.it>
2020-01-31 18:04:22 +0100

    vanilla js

--- ATM/script.js
+++ ATM/script.js
@@ -6,6 +6,11 @@ var searchParams = new URLSearchParams(window.location.search);
 var fermate = [];
 var stazioni = [];
 
+const home = document.querySelector('#home');
+const loader = document.querySelector('.loader');
+const stazionicont = document.querySelector('#stazioni');
+const fermatecont = document.querySelector('#fermate');
+
 if (searchParams.has('martina')) {
     // preimpostato per martina
     fermate.push(10335,10332,10326);
@@ -50,35 +55,38 @@ stazioni.sort();
 
 if (fermate.length == 0 && stazioni.length == 0) {
     // se dopo tutto sto giro non ci sono comunque fermate e stazioni creo la home con le info
-    html = "<div class='home'>";
-        html+= "<h1>Recupero orari bus, tram e treni</h1>";
-        html+= "<div class='bg'><img class='tram-albero' src='img/albero.svg'><img class='tram-banchina' src='img/banchina.svg'><div class='tram-strada'></div><div class='tram'><img class='tram-body vibrate-1' src='img/tram-body.svg'><img class='tram-bg' src='img/tram-bg.svg'></div><div class='tram-filo'></div><img class='tram-albero front' src='img/albero.svg'><div class='tram-erba front'></div></div>";
-        html+= "<h2>Come funziona per le fermate ATM</h2>";
-        html+= '<p>Questo tool serve a recuperare i tempi di attesa delle fermate ATM specificate, per usarlo basta inserire nella url le fermate richieste usando la stringa "<strong>?fermate=10000,20000,30000</strong>" sostituendo i numeri con le fermate preferite, sempre separate da una virgola come questo link di esempio:</p>';
-        html+= '<p><strong><a href="?fermate=14187,14188">?fermate=14187,14188</a></strong></p>';
-        html+= '<blockquote>Il codice per le fermate ATM è di cinque cifre ed è sempre mostrato sulle banchine e sulle fermate, le metro non sono incluse</blockquote>';
-        html+= "<h2>Come funziona per le stazioni Trenitalia e Trenord</h2>";
-        html+= '<p>È possibile anche visualizzare il tabellone dei treni in partenza per le stazioni Trenitalia e Trenord, inserendo il codice univoco nella url usando questa stringa "<strong>?stazioni=S10000,S20000,S30000</strong>", si possono filtrare i risultati inserendo le destinazioni preferite dopo il codice stazione usando i due punti, gli ulteriori filtri aggiuntivi vanno separati dal punto e virgola "<strong>?stazioni=S10000:roma,S20000:torino;parma,S30000</strong>":</p>';
-        html+= '<p><strong><a href="?stazioni=S01630:saronno,S01700:torino;roma">?stazioni=S01630:saronno,S01700:torino;roma</a></strong></p>';
-        html+= '<p>Ovviamente le due informazioni possono essere unite nella url usando il carattere <strong>&</strong> in modo da separare fermate e stazioni: "<strong>?fermate=10000,20000&stazioni=S10000:roma;bologna,S20000:torino,S30000</strong>": </p>';
-        html+= '<p><strong><a href="?fermate=14187,14188&stazioni=S01630,S01700:torino;roma">?fermate=14187,14188&stazioni=S01630,S01700:torino;roma</a></strong></p>';
-        html+= '<blockquote>Il codice per le stazioni dei treni inizia sempre con una lettera (S o N) seguita da 5 cifre e può essere recuperato <a target="_blank" href="https://github.com/sabas/trenitalia/blob/master/stazioni.tsv">in questo documento</a></blockquote>';
-    html+= "</div>";
-    $('#home').append(html);
+    let html = document.createElement('div');
+    html.classList.add("home");
+
+        html.innerHTML = "<h1>Recupero orari bus, tram e treni</h1>";
+        html.innerHTML += "<div class='bg'><img class='tram-albero' src='img/albero.svg'><img class='tram-banchina' src='img/banchina.svg'><div class='tram-strada'></div><div class='tram'><img class='tram-body vibrate-1' src='img/tram-body.svg'><img class='tram-bg' src='img/tram-bg.svg'></div><div class='tram-filo'></div><img class='tram-albero front' src='img/albero.svg'><div class='tram-erba front'></div></div>";
+        html.innerHTML += "<h2>Come funziona per le fermate ATM</h2>";
+        html.innerHTML += '<p>Questo tool serve a recuperare i tempi di attesa delle fermate ATM specificate, per usarlo basta inserire nella url le fermate richieste usando la stringa "<strong>?fermate=10000,20000,30000</strong>" sostituendo i numeri con le fermate preferite, sempre separate da una virgola come questo link di esempio:</p>';
+        html.innerHTML += '<p><strong><a href="?fermate=14187,14188">?fermate=14187,14188</a></strong></p>';
+        html.innerHTML += '<blockquote>Il codice per le fermate ATM è di cinque cifre ed è sempre mostrato sulle banchine e sulle fermate, le metro non sono incluse</blockquote>';
+        html.innerHTML += "<h2>Come funziona per le stazioni Trenitalia e Trenord</h2>";
+        html.innerHTML += '<p>È possibile anche visualizzare il tabellone dei treni in partenza per le stazioni Trenitalia e Trenord, inserendo il codice univoco nella url usando questa stringa "<strong>?stazioni=S10000,S20000,S30000</strong>", si possono filtrare i risultati inserendo le destinazioni preferite dopo il codice stazione usando i due punti, gli ulteriori filtri aggiuntivi vanno separati dal punto e virgola "<strong>?stazioni=S10000:roma,S20000:torino;parma,S30000</strong>":</p>';
+        html.innerHTML += '<p><strong><a href="?stazioni=S01630:saronno,S01700:torino;roma">?stazioni=S01630:saronno,S01700:torino;roma</a></strong></p>';
+        html.innerHTML += '<p>Ovviamente le due informazioni possono essere unite nella url usando il carattere <strong>&</strong> in modo da separare fermate e stazioni: "<strong>?fermate=10000,20000&stazioni=S10000:roma;bologna,S20000:torino,S30000</strong>": </p>';
+        html.innerHTML += '<p><strong><a href="?fermate=14187,14188&stazioni=S01630,S01700:torino;roma">?fermate=14187,14188&stazioni=S01630,S01700:torino;roma</a></strong></p>';
+        html.innerHTML += '<blockquote>Il codice per le stazioni dei treni inizia sempre con una lettera (S o N) seguita da 5 cifre e può essere recuperato <a target="_blank" href="https://github.com/sabas/trenitalia/blob/master/stazioni.tsv">in questo documento</a></blockquote>';
+
+    home.appendChild(html);
+    
 } else {
     // prima di tutto mostro il loader
-    $('.loader').addClass('show');
+    loader.classList.add("show");
     // mi segno l'ora della chiamata
     let time = ora();
     // se ci sono fermate le chiamo in ordine
     if (fermate) {
-        $('#fermate').attr("data-time",time.time);
+        fermatecont.setAttribute("data-time", time.time);
         fermate.forEach(function(fermata) {
             chiamafermata(fermata, 'stops');
         });
     }
     if (stazioni) {
-        $('#stazioni').attr("data-time",time.time);
+        stazionicont.setAttribute("data-time", time.time);
         stazioni.forEach(function(stazione) {
             let stazionefiltro = [];
             if (stazione.includes(":")) {
@@ -96,95 +104,110 @@ function chiamafermata(fermataid, tipo) {
     let fd = new FormData();
     fd.append("url", "tpPortal/geodata/pois/" + tipo + "/" + fermataid);
     // creo i div per ospitare le fermate in ordine
-    html = "<table class='fermata' data-id='" + fermataid + "'></table>";
-    $('#fermate').append(html);
-    $.ajax({
-        type: "POST",
-        url: cors + APIurl.ATM,
-        crossDomain: true,
-        contentType: false,
-        processData: false,
-        data: fd,
-        success: function (data) {
-            if (data['Lines'].length > 0) {
-                // alla prima risposta tolgo il loader
-                $('.loader').removeClass('show');
-                $('#fermate').addClass('ready');
-                // do something with server response data
-                creafermata(fermataid, data);
-            }
 
-        },
-        error: function (err) {
-            $('.loader').removeClass('show');
-            html = "<div class='home'>";
-            if(err.status == 404) {
-                // fermata non raggiungibile
-                html+= '<p>La fermata richiesta <b>' + fermataid + '</b> non è raggiungibile, controlla che il numero sia corretto prima di ricaricare la pagina</p>';
-            } else {
-                // errore generico
-                html+= '<p>Qualcosa è andato storto, prova a ricaricare e ad incrociare le dita. Giuro che di solito funziona!</p>';
-            }
-            html+= "</div>";
-            $('#home').append(html);
-            // handle your error logic here
-        }
-    });
-}
-function chiamastazione(stazioneid, date, filtro) {
-    html = "<table class='stazione' data-id='" + stazioneid + "'></tables>";
-    $('#stazioni').append(html);
-    // http://www.viaggiatreno.it/viaggiatrenonew/resteasy/viaggiatreno/dettaglioStazione/S01700/1
-    $.ajax({
-        type: "GET",
-        url: cors + APIurl.FFSS + 'dettaglioStazione/' + stazioneid + '/1',
-        success: function (data) {
+    let html = document.createElement('table');
+    html.classList.add("fermata")
+    html.setAttribute("data-id", fermataid);
+
+    fermatecont.appendChild(html);
+    
+    fetch(cors + APIurl.ATM, {
+        method: 'POST',
+        body: fd
+    })
+    .then(response => response.json())
+    .then(function(data) {
+        if (data['Lines'].length > 0) {
+            // alla prima risposta tolgo il loader
+            loader.classList.remove("show");
+            fermatecont.classList.add("ready");
             // do something with server response data
-            chiamapartenze(stazioneid, date, data, filtro);
-        },
-        error: function (err) {
-            $('.loader').removeClass('show');
-            html = "<div class='home'>";
-            if(err.status == 404) {
-                // fermata non raggiungibile
-                html+= '<p>La stazione richiesta <b>' + stazioneid + '</b> non è raggiungibile, controlla che il numero sia corretto prima di ricaricare la pagina</p>';
-            } else {
-                // errore generico
-                html+= '<p>Qualcosa è andato storto, prova a ricaricare e ad incrociare le dita. Giuro che di solito funziona!</p>';
-            }
-            html+= "</div>";
-            $('#home').append(html);
-            // handle your error logic here
+            creafermata(fermataid, data);
         }
+    })
+    .catch(function (err) {
+        loader.classList.remove("show");
+        
+        let html = document.createElement('div');
+        html.classList.add("home");
+        if(err.status == 404) {
+            // fermata non raggiungibile
+            html.innerHTML = '<p>La fermata richiesta <b>' + fermataid + '</b> non è raggiungibile, controlla che il numero sia corretto prima di ricaricare la pagina</p>';
+            console.log(err);
+        } else {
+            // errore generico
+            html.innerHTML = '<p>Qualcosa è andato storto, prova a ricaricare e ad incrociare le dita. Giuro che di solito funziona!</p>';
+            console.log(err);
+        }
+        home.appendChild(html);
+        // handle your error logic here
     });
 }
-function chiamapartenze(stazioneid, date, infostazione, filtro) {
-    $.ajax({
-        type: "GET",
-        url: cors + APIurl.FFSS + 'partenze/' + stazioneid + '/' + date.full,
-        success: function (data) {
-            if (data.length > 0) {
-                // alla prima risposta tolgo il loader
-                $('.loader').removeClass('show');
-                $('#stazioni').addClass('ready');
-                // do something with server response data
-                creastazione(stazioneid, data, infostazione, filtro);
-            }
-        },
-        error: function (err) {
-            $('.loader').removeClass('show');
-            html = "<div class='home'>";
-            if(err.status == 404) {
-                // fermata non raggiungibile
-                html+= '<p>La stazione richiesta <b>' + stazioneid + '</b> non è raggiungibile, controlla che il numero sia corretto prima di ricaricare la pagina</p>';
-            } else {
-                // errore generico
-                html+= '<p>Qualcosa è andato storto, prova a ricaricare e ad incrociare le dita. Giuro che di solito funziona!</p>';
-            }
-            html+= "</div>";
-            $('#home').append(html);
-            // handle your error logic here
+
+function chiamastazione(stazioneid, date, filtro) {
+    let html = document.createElement('table');
+    html.classList.add("stazione")
+    html.setAttribute("data-id", stazioneid);
+
+    stazionicont.appendChild(html);
+
+    fetch(cors + APIurl.FFSS + 'dettaglioStazione/' + stazioneid + '/1', {
+        method: 'GET'
+    })
+    .then(response => response.json())
+    .then(function(data) {
+        // do something with server response data
+        chiamapartenze(stazioneid, date, data, filtro);
+    })
+    .catch(function (err) {
+        loader.classList.remove("show");
+        
+        let html = document.createElement('div');
+        html.classList.add("home");
+        if(err.status == 404) {
+            // fermata non raggiungibile
+            html.innerHTML = '<p>La stazione richiesta <b>' + stazioneid + '</b> non è raggiungibile, controlla che il numero sia corretto prima di ricaricare la pagina</p>';
+            console.log(err);
+        } else {
+            // errore generico
+            html.innerHTML = '<p>Qualcosa è andato storto, prova a ricaricare e ad incrociare le dita. Giuro che di solito funziona!</p>';
+            console.log(err);
         }
+        home.appendChild(html);
+        // handle your error logic here
+    });
+}
+
+function chiamapartenze(stazioneid, date, infostazione, filtro) {
+    fetch(cors + APIurl.FFSS + 'partenze/' + stazioneid + '/' + date.full, {
+        method: 'GET'
+    })
+    .then(response => response.json())
+    .then(function(data) {
+        if (data.length > 0) {
+            // alla prima risposta tolgo il loader
+            loader.classList.remove("show");
+            stazionicont.classList.add("ready");
+            // do something with server response data
+            creastazione(stazioneid, data, infostazione, filtro);
+        }
+    })
+    .catch(function (err) {
+        loader.classList.remove("show");
+        
+        let html = document.createElement('div');
+        html.classList.add("home");
+        if(err.status == 404) {
+            // fermata non raggiungibile
+            html.innerHTML = '<p>La stazione richiesta <b>' + stazioneid + '</b> non è raggiungibile, controlla che il numero sia corretto prima di ricaricare la pagina</p>';
+            console.log(err);
+        } else {
+            // errore generico
+            html.innerHTML = '<p>Qualcosa è andato storto, prova a ricaricare e ad incrociare le dita. Giuro che di solito funziona!</p>';
+            console.log(err);
+        }
+        home.appendChild(html);
+        // handle your error logic here
     });
 }
 
@@ -203,49 +226,63 @@ function ora() {
 }
 
 function creafermata(id, info) {
-    html = "<thead>";
-    html += "<th colspan='3' class='fermata-title'>" + id + " - " + info['Description'] + "</th>";
-    html += "<tr><th class='numero'>#</th><th class='direzione'>Direzione</th><th class='orario'>Orario</th></tr>";
-    html += "</thead>";
-    html += "<tbody>";
+
+    let singlefermata = document.querySelector('.fermata[data-id="' + id + '"]');
+
+    let html = document.createElement('thead');
+
+    html.innerHTML += "<th colspan='3' class='fermata-title'>" + id + " - " + info['Description'] + "</th>";
+    html.innerHTML += "<tr><th class='numero'>#</th><th class='direzione'>Direzione</th><th class='orario'>Orario</th></tr>";
+
+    singlefermata.appendChild(html);
+
+    let html2 = document.createElement('tbody');
+    let string = '';
+
     info['Lines'].forEach(function(linea) {
-        html+= "<tr class='linea'>";
-        html+= "<td class='numero'>" + linea['Line']['LineCode'] + "</td>";
-        html+= "<td class='direzione'>" + linea['Line']['LineDescription'].split(" - ").pop() + "</td>";
-        html+= "<td class='orario'>" + linea['WaitMessage'] + "</td>";
-        html+= "</tr>";
+        string += "<tr class='linea'>";
+        string += "<td class='numero'>" + linea['Line']['LineCode'] + "</td>";
+        string += "<td class='direzione'>" + linea['Line']['LineDescription'].split(" - ").pop() + "</td>";
+        string += "<td class='orario'>" + linea['WaitMessage'] + "</td>";
+        string += "</tr>";
+        html2.innerHTML = string;
     });
-    html+= "</tbody>";
-    $('.fermata[data-id=' + id + ']').append(html);
+
+    singlefermata.appendChild(html2);
+
 }
 
 function creastazione(id, info, infostazione, filtro) {
     if (info[0]) { // se c'e' almeno una linea
-        html = "<thead>";
-        html += "<th colspan='6' class='stazione-title'>" + id + " - " + infostazione.localita.nomeBreve + "</th>";
-        html += "<tr><th class='numero'>#</th><th class='direzione'>Direzione</th><th class='orario'>Orario</th><th class='ritardo'>Ritardo</th><th class='stato'>Stato</th></tr>";
-        html += "</thead>";
-        html += "<tbody>";
-        info.forEach(function(linea) {
-            if (filtro.length == 0 || filtro.some(filtrino=>linea.destinazione.includes(filtrino.toUpperCase()))) {
-            // se non ci sono filtri o se il filtro corrisponde entriamo dentro e stampiamo il treno
-            // if (linea.destinazione.includes(filtro.toUpperCase())) {
-                html+= "<tr class='linea'>";
-                html+= "<td class='numero'>" + linea.categoria + ' ' +  linea.numeroTreno + "</td>";
-                html+= "<td class='direzione'>" + linea.destinazione + "</td>";
-                html+= "<td class='orario'>" + linea.compOrarioPartenzaZeroEffettivo + "</td>";
-                html+= "<td class='ritardo'>" + linea.ritardo + " MIN</td>";
-                html+= "<td class='stato'>" + linea.compInStazionePartenza[0] + "</td>";
-                html+= "</tr>";
-            }
-        });
-        html+= "</tbody>";
-        $('.stazione[data-id=' + id + ']').append(html);
-    }
-}
 
-function chiamatreno(stazioneid) {
+    let singlestazione = document.querySelector('.stazione[data-id="' + id + '"]');
+
+    let html = document.createElement('thead');
+
+    html.innerHTML += "<th colspan='6' class='stazione-title'>" + id + " - " + infostazione.localita.nomeBreve + "</th>";
+    html.innerHTML += "<tr><th class='numero'>#</th><th class='direzione'>Direzione</th><th class='orario'>Orario</th><th class='ritardo'>Ritardo</th><th class='stato'>Stato</th></tr>";
+
+    singlestazione.appendChild(html);
+
+    let html2 = document.createElement('tbody');
+    let string = '';
+
+    info.forEach(function(linea) {
+        if (filtro.length == 0 || filtro.some(filtrino=>linea.destinazione.includes(filtrino.toUpperCase()))) {
+            string += "<tr class='linea'>";
+            string += "<td class='numero'>" + linea.categoria + ' ' +  linea.numeroTreno + "</td>";
+            string += "<td class='direzione'>" + linea.destinazione + "</td>";
+            string += "<td class='orario'>" + linea.compOrarioPartenzaZeroEffettivo + "</td>";
+            string += "<td class='ritardo'>" + linea.ritardo + " MIN</td>";
+            string += "<td class='stato'>" + linea.compInStazionePartenza[0] + "</td>";
+            string += "</tr>";
+            html2.innerHTML = string;
+        }
+    });
+
+    singlestazione.appendChild(html2);
     
+    }
 }
 
 // API Treni
