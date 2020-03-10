946a66ccbb98bb0871bd9f51541d54fbaa2b4e3f
Federico Lelli <federico.lelli@mondadori.it>
2020-03-10 17:32:38 +0100

    mensa

--- MENSA/script.js
+++ MENSA/script.js
@@ -3,26 +3,76 @@ let unixtime = Math.round(+new Date()/1000);
 let today = new Date().getDay();
 
 const cors = 'https://cors-anywhere.herokuapp.com/';
-let url = 'https://mondadori.pellegrinicloud.it/menu/'+ unixtime + '/0/368/3';
+let url = 'https://mondadori.pellegrinicloud.it/menu/'+ unixtime + '/0/368';
+let welnessurl = 'https://mondadori.pellegrinicloud.it/menu/'+ unixtime + '/0/370';
 // inizializzo l'array che conterrà il menu
-let menu = [];
 
 console.log(url);
+// recupero i menù
 
-fetch(cors + url, {
-        method: 'GET'
-    })
-    .then(response => response.text())
-    .then(function(data) {
-        // console.log(data);
-        getinfo(data);
-        // arrivati a questo punto abbiamo tutto il menu del giorno
-        console.log(menu);
+chiamatamenu(url);
+chiamatamenu(welnessurl);
 
+function chiamatamenu(link) {
+    let menu = [];
+    // chiamo la pagina e faccio scraping delle info
+    // stampa a schermo direttamente in menu
+    fetch(cors + link, {
+            method: 'GET'
+        })
+        .then(response => response.text())
+        .then(function(data) {
+            // console.log(data);
+            getinfo(data);
+            // arrivati a questo punto abbiamo tutto il menu del giorno
+            // console.log(menu);
+            // console.log(menuwelness);
+            stampomenu(menu);
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
+
+    // console.log(cors);
+
+    function getinfo(body) {
+        // creo html virtuale dalla chiamata GET per cercare nel dom
+        let cachedEl = document.createElement('html');
+        cachedEl.innerHTML = body;
+        // isolo le portate
+        let portate = cachedEl.querySelectorAll(".tabella_menu_settimanale td");    
+        portate.forEach(portata);
+        function portata(index) {
+            let piatti = index.querySelectorAll(".piatto_inline");
+            index.dataset.giorno;
+            index.dataset["tipo-piatto"];
+            let tipopiatto = index.getAttribute("data-tipo-piatto");
+            let giornopiatto = index.dataset.giorno;
+            // isolo i singoli piatti
+            piatti.forEach(singolopiatto);
+            function singolopiatto(index, value) {
+                if (giornopiatto == today) {
+                    // prendo solo i piatti del giorno
+                    piatto = {};
+                    piatto.nome = index.querySelector("a.cbp-singlePage img").alt;
+                    piatto.tipo = tipopiatto;
+                    piatto.giorno = giornopiatto;
+                    piatto.url = index.querySelector("a.cbp-singlePage").href;
+                    piatto.img = index.querySelector("a.cbp-singlePage img").src;
+                    // console.log(piatto);
+                    // salvo tutto come un array di oggetti
+                    menu.push(piatto);
+                }
+            }
+        }
+    }
+
+    function stampomenu(menu) {
+        // menu è un array che contiene un oggetto per ogni piatto
         // stampo i risultati
         const home = document.querySelector('#home');
         let html = document.createElement('div');
-        html.classList.add("home");
+        html.classList.add('home');
 
         let string = '';
         menu.forEach(function(piatto) {
@@ -33,42 +83,7 @@ fetch(cors + url, {
         })
         html.innerHTML = string;
         home.appendChild(html);
-    })
-    .catch(function(err) {
-        console.log(err);
-    });
-
-// console.log(cors);
-
-function getinfo(body) {
-    // creo html virtuale dalla chiamata GET per cercare nel dom
-    let cachedEl = document.createElement('html');
-    cachedEl.innerHTML = body;
-    // isolo le portate
-    let portate = cachedEl.querySelectorAll(".tabella_menu_settimanale td");    
-    portate.forEach(portata);
-    function portata(index) {
-        let piatti = index.querySelectorAll(".piatto_inline");
-        index.dataset.giorno;
-        index.dataset["tipo-piatto"];
-        let tipopiatto = index.getAttribute("data-tipo-piatto");
-        let giornopiatto = index.dataset.giorno;
-        // isolo i singoli piatti
-        piatti.forEach(singolopiatto);
-        function singolopiatto(index, value) {
-            if (giornopiatto == today) {
-                // prendo solo i piatti del giorno
-                piatto = {};
-                piatto.nome = index.querySelector("a.cbp-singlePage img").alt;
-                piatto.tipo = tipopiatto;
-                piatto.giorno = giornopiatto;
-                piatto.url = index.querySelector("a.cbp-singlePage").href;
-                piatto.img = index.querySelector("a.cbp-singlePage img").src;
-                // console.log(piatto);
-                // salvo tutto come un array di oggetti
-                menu.push(piatto);
-            }
-        }
     }
 }
+
 
