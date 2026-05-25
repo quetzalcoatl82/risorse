37850292c14b189ff2bb17ff48418b5bae32505d
Federico Lelli <federico.lelli@mondadori.it>
2026-05-25 16:42:09 +0200

    fix img

--- adv/hpsponsor.v3.js
+++ adv/hpsponsor.v3.js
@@ -110,7 +110,6 @@ function loadSkinWeb(colore_sfondo_w,img_web_w,posizione_w,altezza_testata_w,url
 		'--mm-skin-top-margin': topMarginSkin + 'px',
 		'--mm-skin-content-offset': maginTopContenitore + 'px',
 		'--mm-skin-content-offset-low': maginTopContenitoreLow + 'px',
-		'--mm-skin-bg-image': 'url(' + img_web_w + ')',
 		'--mm-skin-bg-color': colore_sfondo_w === '#fdfaf9' ? '' : colore_sfondo_w,
 		'--mm-skin-bg-size': '1764px auto',
 		'--mm-skin-bg-size-md': '1471px auto'
@@ -131,7 +130,7 @@ function loadSkinWeb(colore_sfondo_w,img_web_w,posizione_w,altezza_testata_w,url
 	if(gapHeight>=scrollPosInitial) // non faccio espandere il container se l'utente ha scrollato
 	cssSkinWeb += '#contenitore-sito-x-adv{width:1200px; margin:0 auto;}';
 	
-	cssSkinWeb += '.mh2021Page {background-image: var(--mm-skin-bg-image) !important; background-color: var(--mm-skin-bg-color) !important; background-repeat: no-repeat !important;background-position-x: center !important;background-position-y: var(--mm-skin-top-margin)!important;background-size:var(--mm-skin-bg-size) !important;}';
+	cssSkinWeb += '.mh2021Page {background-image: url(' + img_web_w + ')!important; background-color: var(--mm-skin-bg-color) !important; background-repeat: no-repeat !important;background-position-x: center !important;background-position-y: var(--mm-skin-top-margin)!important;background-size:var(--mm-skin-bg-size) !important;}';
 	
 	cssSkinWeb += '@media only screen and (max-width: 1440px) and (min-width: 1025px) { body,.mh2021Page{background-size:var(--mm-skin-bg-size-md) !important;}}';
 		
@@ -223,7 +222,6 @@ function loadSkinWeb2(configSkin){
 		'--mm-skin-top-margin': topMarginSkin + 'px',
 		'--mm-skin-content-offset': maginTopContenitore + 'px',
 		'--mm-skin-content-offset-low': maginTopContenitoreLow + 'px',
-		'--mm-skin-bg-image': 'url(' + configSkin.img_web + ')',
 		'--mm-skin-bg-color': configSkin.colore_sfondo === '#fdfaf9' ? '' : configSkin.colore_sfondo,
 		'--mm-skin-bg-size': '1764px auto',
 		'--mm-skin-bg-size-md': '1471px auto'
@@ -239,7 +237,7 @@ function loadSkinWeb2(configSkin){
 	if(gapHeight>=scrollPosInitial) // non faccio espandere il container se l'utente ha scrollato
 	cssSkinWeb += '#contenitore-sito-x-adv{height:var(--mm-skin-content-offset) !important;width:1200px;margin:0 auto;-webkit-transition:height 0.5s ease-out;-moz-transition:height 0.5s ease-out;-o-transition:height 0.5s ease-out;transition:height 0.5s ease-out;}';
 	
-	cssSkinWeb += '.mh2021Page {background-image:var(--mm-skin-bg-image) !important;background-color:var(--mm-skin-bg-color,transparent) !important;background-repeat:no-repeat !important;background-position-x:center !important;background-position-y:var(--mm-skin-top-margin) !important;background-size:var(--mm-skin-bg-size) !important;}';
+	cssSkinWeb += '.mh2021Page {background-image:url(' + img_web_w + ')!important;background-color:var(--mm-skin-bg-color,transparent) !important;background-repeat:no-repeat !important;background-position-x:center !important;background-position-y:var(--mm-skin-top-margin) !important;background-size:var(--mm-skin-bg-size) !important;}';
 	
 	cssSkinWeb += '@media only screen and (max-width: 1440px) and (min-width: 1025px) { body,.mh2021Page{background-size:var(--mm-skin-bg-size-md) !important;}#contenitore-sito-x-adv{height:var(--mm-skin-content-offset-low)}}';
 		
